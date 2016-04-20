10c44e0e6d4507037473d0cadb0b974a23f63ec0
Joshua Gustafson and Michael Schennum <silversevenfox@yahoo.com>
2016-04-20 09:37:28 -0700

    adds array list

--- app/src/main/java/com/example/guest/boggle/MainActivity.java
+++ app/src/main/java/com/example/guest/boggle/MainActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import butterknife.Bind;
@@ -13,9 +17,9 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.rollButton) Button mRollButton;
+    @Bind(R.id.randomTextView) TextView mRandomTextView;
     public static final String TAG = MainActivity.class.getSimpleName();
-    final String alphabet = "abcdefghijklmnopqrstuvwxyz";
-    final int N = alphabet.length();
+    List<String> randomLetters = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +29,15 @@ public class MainActivity extends AppCompatActivity {
 
         mRollButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Random r = new Random();
-                char c = (char) (r.nextInt(26) + 'a');
-                String myLetter = "" + c;
-                Log.d(TAG, myLetter);
+                randomLetters.clear();
+                String location = mRandomTextView.getText().toString();
+                for(int i=0; i < 8; i++) {
+                    Random r = new Random();
+                    char c = (char) (r.nextInt(26) + 'a');
+                    String myLetter = "" + c;
+                    randomLetters.add(myLetter);
+                }
+            Log.d(TAG, "" + randomLetters);
             }
         });
     }
